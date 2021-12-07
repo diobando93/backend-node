39a0e088e3e3f25c85f8293f6ddc9a70eba16218
Israel Obando <devisrael93@gmail.com>
2021-12-07 17:37:13 +0100

    message, creado con su respectivo network, falta guardar en una BD

---
+++ components/message/controller.js
@@ -0,0 +1,19 @@
+function addMessage(user, message){
+    return new Promise((resolve, reject) => {
+        if(!user || !message){
+            console.error('[messageConstroller] No hay usuario o mensaje')
+            reject('Los datos son incorrectos');
+            return false;
+        }
+        const fullMessage = {
+            user: user,
+            message: message,
+            date: new Date(),
+        }
+        console.log(fullMessage);
+        resolve(fullMessage);
+    })
+}
+module.exports = {
+    addMessage,
+}
---
+++ components/message/network.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const response = require('../../network/response');
+const controller = require('./controller');
+
+router.get('/', function(req, res) {
+    console.log(req.headers);
+    res.header({
+        "custom-header": "Nuestro valor personalizado"
+    })
+    response.success(req, res, 'lista de mensajes');
+});
+
+router.post('/', function(req, res) {
+    controller.addMessage(req.body.user, req.body.message)
+        .then((fullMessage) => {
+            response.success(req, res, fullMessage, 201);
+        })
+        .catch(e => {
+            response.error(req, res, 'Informacion invalida', 400, 'Error en el controlador');
+        });
+});
+
+module.exports = router;
